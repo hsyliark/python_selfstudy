f20b9761f459536cf1507da054dbb0046bceca26
hsyliark <sungyoonhwang3@gmail.com>
2019-08-11 18:46:26 +0900

    add

--- Basic programming/코딩도장/코딩도장 28.py
+++ Basic programming/코딩도장/코딩도장 28.py
@@ -1,1 +1,13 @@
-#
+# 홀수와 짝수의 개수를 구하는 프로그램을 만들어라.
+
+num_list = list(map(int, input('임의의 양의 정수를 입력하세요> ').split()))
+def odd_even(num_list):
+    odd = 0
+    even = 0
+    for i in range(len(num_list)):
+        if num_list[i] % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return '홀수 ' + str(odd) + '개, ' + '짝수 ' + str(even) + '개'
+odd_even(num_list)
