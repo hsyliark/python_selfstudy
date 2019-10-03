527a406d395ae681f60d88aa105876a3be0ad1e9
hsyliark <sungyoonhwang3@gmail.com>
2019-10-03 18:48:36 +0900

    add

--- Basic programming/나의 첫 파이썬/chapter 1~2.py
+++ Basic programming/나의 첫 파이썬/chapter 1~2.py
@@ -2,3 +2,30 @@ print("Hello Python interpreter!")
 print("Hello world!")
 message = "Hello Python Crash Course world!"
 print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = ' python '
+favorite_language
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
