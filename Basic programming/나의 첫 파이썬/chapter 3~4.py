605ba2e26c8540a3e13ca72c7cfbd71c47b1aa1e
hsyliark <sungyoonhwang3@gmail.com>
2019-10-09 19:23:22 +0900

    add

--- Basic programming/나의 첫 파이썬/chapter 3~4.py
+++ Basic programming/나의 첫 파이썬/chapter 3~4.py
@@ -5,3 +5,30 @@ print(bicycles[0].title())
 print(bicycles[-1])
 message = "My first bicycle was a " + bicycles[0].title() + "."
 print(message)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[1]
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+print("The last motorcycle I owned was a " + popped_motorcycle.title() + ".")
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a " + first_owned.title() + ".")
+
